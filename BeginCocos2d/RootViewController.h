d4f8d056e19ccb906c4ee43c2d2f628bd67384b7
shin <suoxin117@gmail.com>
2012-10-15 11:26:31 +0800

    Initial Commit

---
+++ BeginCocos2d/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BeginCocos2d
+//
+//  Created by shin on 12-10-15.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ BeginCocos2d/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BeginCocos2d
+//
+//  Created by shin on 12-10-15.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
